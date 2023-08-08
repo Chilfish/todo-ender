9678bcb32debf094a9fd8b982bc467f0d0b103bb
Chilfish <chill4fish@gmail.com>
2023-08-08 16:31:27 +0800

    lint

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { add } from './_utils'
+import { add } from './_utils.js'
 
 export default function handler(
   req: VercelRequest,
